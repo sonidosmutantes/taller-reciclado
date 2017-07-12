1a8495c1ab2fa9551a09eeb898d1dbe50a08e7a1
Hernán Ordiales <hordiales@gmail.com>
2017-07-12 01:35:44 -0300

    fix  127.0.0.1:57120

--- controllers_ui/run_osc_app_sc_port.sh
+++ controllers_ui/run_osc_app_sc_port.sh
@@ -1,3 +1,3 @@
 echo "Bind a 57120, puerto OSC default de SuperCollider"
 # Si esta ocupado, SC intenta usar 57121
-open-stage-control -n -l OpenStageControl_UI/synth.js -s 127.0.0.1:5555 127.0.0.1:57120
+open-stage-control -n -l OpenStageControl_UI/synth.js -s 127.0.0.1:57120
